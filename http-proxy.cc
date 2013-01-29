be029fb68e698da6ee38d52d8e11c7f843b141a6
Minjian Wang <minjian@lnxsrv02.seas.ucla.edu>
2013-01-28 20:39:06 -0800

    Adding getaddrinfo and checked command-line parameters

--- http-proxy.cc
+++ http-proxy.cc
@@ -1,11 +1,21 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 using namespace std;
 
 int main (int argc, char *argv[])
 {
+  //http-proxy called without command-line parameters
+  if (argc != 0)
+    {
+      cout << "Cannot include parameters with http-proxy call";
+      return 1;
+    }
+
   // command line parsing
   
   return 0;
