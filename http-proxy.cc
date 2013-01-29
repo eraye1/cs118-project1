60baa1c52ceb66f47995ee64a6c2e4998d3e2986
Minjian Wang <minjian@lnxsrv02.seas.ucla.edu>
2013-01-28 22:36:07 -0800

    Check the README file for "Current Issues".  Currently hangs on listen or somewhere around there.  A little after that but it seems like we can't output the listen fd.  Also possible something funny happened with the cout and putting in multiple ints and strings in sequence.  Changes are all in http-proxy.  See README for more on how to run it.  Make sure the paths are imported in else you get funny errors.

--- http-proxy.cc
+++ http-proxy.cc
@@ -1,22 +1,47 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include <iostream>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
 
 using namespace std;
 
 int main (int argc, char *argv[])
 {
   //http-proxy called without command-line parameters
-  if (argc != 0)
+  if (argc != 1)
     {
-      cout << "Cannot include parameters with http-proxy call";
+      cout << "Cannot include parameters with http-proxy call\n";
       return 1;
     }
 
-  // command line parsing
+
+  //making sockets like we're sock-puppets
+  int sock_d;
+  struct addrinfo hostai, *result;
+  struct sockaddr_storage their_addr;
+  
+  memset(&result,0,sizeof result);
+  hostai.ai_family = AF_INET;  //IPv4, but not sure if we need to do IPv6?
+  hostai.ai_socktype = SOCK_STREAM;  //stream socket, uses TCP
+  hostai.ai_flags = AI_PASSIVE;  //fill in IP automatically, useful for binding
+
+  getaddrinfo(NULL, "14805", &hostai, &result);  //take the flags set in hostai and create an addrinfo with those values and flags stuff made.
+  
+  sock_d = socket(result->ai_family,result->ai_socktype,0);  //create socket with hostai properties, 0 for the protocol field automatically chooses the correct protocol
+  bind(sock_d, result->ai_addr, result-> ai_addrlen); //bind socket to result addrinfo
+
+  int listenresult = listen(sock_d,15);  //silently limits incoming queue to 15
+
+  //print the sd because warnings treated as errors
+  cout << "socketfd: " << sock_d << "\nlistenresult: " << listenresult;
+
+  socklen_t addr_size = sizeof their_addr;
+  int acceptres = accept(sock_d, (struct sockaddr *)&their_addr, &addr_size);  //accept the connection
+  cout <<  "\nacceptres: " << acceptres << "\n";  
   
   return 0;
 }
